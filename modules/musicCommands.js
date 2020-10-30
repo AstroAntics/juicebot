f75dd84fd7b1a71134cfb4a10e266d9a1a1dfa47
SilverCrown <54302245+AstroAntics@users.noreply.github.com>
2020-10-30 20:19:15 +0530

    Update musicCommands.js

--- modules/musicCommands.js
+++ modules/musicCommands.js
@@ -2,19 +2,23 @@ const { client, PREFIX } = require('../index'); // Import the client from index.
 const Discord = require('discord.js');
 const MusicBot = require('discord-music-system');
  
-const bot = new MusicBot({
+const bot = new MusicBot(
+ {
     botPrefix: PREFIX,
     ytApiKey: process.env.YT_APIKEY,
     botClient: client
 });
 
-client.on("message", async (message) => {
+client.on("message", async (message) => 
+{
 
     if (message.author.bot) return;
     if (!message.guild) return;
+ 
     if (!message.content.startsWith(PREFIX)) return;
 
-    if (message.content.toLowerCase().startsWith(`${PREFIX}`)) {
+    if (message.content.toLowerCase().startsWith(`${PREFIX}`)) 
+    {
         bot.onMessage(message);
     }
 });
